fbb9b3a537c45fa5b9f945c0d9d53d9e7e5b181b
g32liang <graceliang786@gmail.com>
2026-05-13 20:55:59 -0400

    exit 8
    created the exit 8 design

--- sketch.js
+++ sketch.js
@@ -1,7 +1,28 @@
-function setup() {
-  createCanvas(400, 400);
+let img;
+
+function preload() {
+  img = loadImage("assets/images/nosmoking.jpg");
 }
 
-function draw() {
-  background(0, 0, 255);
+function setup() {
+  createCanvas(400, 400);
+  background(227, 209, 52);
+  image(img, 50, 150);
+
+  textSize(40);
+  stroke(10);
+  text("出口", 50, 70);
+
+  textSize(30);
+  stroke(3);
+  text("Exit", 50, 100);
+
+  textSize(80);
+  stroke(20);
+  text("8", 150, 100);
+
+  fill(0);
+  triangle(280, 60, 320, 60, 300, 40);
+
+  rect(290, 60, 20, 40);
 }

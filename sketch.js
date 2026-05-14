126b58b95ecc70e0587edf848f4b93cdbc366371
g32liang <graceliang786@gmail.com>
2026-05-13 21:06:21 -0400

    moved text over by 10
    moved exit 8 over by 10

--- sketch.js
+++ sketch.js
@@ -11,15 +11,15 @@ function setup() {
 
   textSize(40);
   stroke(10);
-  text("出口", 50, 70);
+  text("出口", 60, 70);
 
   textSize(30);
   stroke(3);
-  text("Exit", 50, 100);
+  text("Exit", 60, 100);
 
   textSize(80);
   stroke(20);
-  text("8", 150, 100);
+  text("8", 160, 100);
 
   fill(0);
   triangle(280, 60, 320, 60, 300, 40);
